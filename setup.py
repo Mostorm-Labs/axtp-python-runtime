9d17aaf87011f57c007d73d2d3d748cef1811a2d
Evan <luckkievan@gmail.com>
2026-06-15 16:12:59 +0800

    chore: upgrade AXTP spec to v0.6.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="axtp-runtime",
-    version="0.5.1",
+    version="0.6.0",
     description="Python runtime primitives for AXTP",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
